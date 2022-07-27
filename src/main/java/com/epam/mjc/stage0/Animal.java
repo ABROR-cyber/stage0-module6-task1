1ca6ebfe1301f25f9e54af92fae98f5aa6c870c7
abror <abror01042001@gmail.com>
2022-07-28 04:01:44 +0500

    fixed

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -5,8 +5,6 @@ public class Animal {
     private int numberOfPaws;
     private boolean hasFur;
 
-    public Animal() {
-    }
 
     public Animal(String color, int numberOfPaws, boolean hasFur) {
         this.color = color;
